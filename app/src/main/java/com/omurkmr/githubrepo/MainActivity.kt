17c6100f196e690a84cfcc4f6695ab99cf3dad98
Ömür Kumru <omur.kumru@exozet.com>
2019-11-11 23:10:27 +0100

    created views

--- app/src/main/java/com/omurkmr/githubrepo/MainActivity.kt
+++ app/src/main/java/com/omurkmr/githubrepo/MainActivity.kt
@@ -2,11 +2,26 @@ package com.omurkmr.githubrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import com.omurkmr.githubrepo.extentions.viewModel
+import com.omurkmr.githubrepo.services.repositories.RepositoryViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val repositoryViewModel by viewModel<RepositoryViewModel>(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setObservers()
+        repositoryViewModel.refreshRepositories("fhg")
+    }
+
+    private fun setObservers() {
+        repositoryViewModel.repositories.observe(this, Observer {
+            Log.i("tttt","${it}")
+        })
     }
 }
